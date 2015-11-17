bb945e2bd7239ee64434d06cb328a7392ba9e7c1
quanit94 <quanit94@gmail.com>
2015-11-17 23:03:58 +0700

    oke

--- public/index.php
+++ public/index.php
@@ -19,7 +19,7 @@
 |
 */
 
-require __DIR__.'../bootstrap/autoload.php';
+require __DIR__.'../../bootstrap/autoload.php';
 
 /*
 |--------------------------------------------------------------------------
@@ -33,7 +33,7 @@ require __DIR__.'../bootstrap/autoload.php';
 |
 */
 
-$app = require_once DIR.'/bootstrap/start.php';
+$app = require_once DIR.'../bootstrap/start.php';
 
 /*
 |--------------------------------------------------------------------------
